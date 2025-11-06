0c684fa037bcd0e8a2eb18e044ad398e97b55481
Ashutosh-star-bit <rajbhawansingh08@gmail.com>
2025-11-06 09:44:01 +0530

    Delete SaveNServe directory

--- SaveNServe/scripts/config.js
+++
@@ -1,6 +0,0 @@
-// config.js - put your Google Maps API key here
-const GOOGLE_MAPS_API_KEY = "REPLACE_WITH_REAL_KEY";
-/*
-To use: in HTML include <script src="https://maps.googleapis.com/maps/api/js?key=REPLACE_WITH_REAL_KEY&callback=initMap" async defer></script>
-Then the app.js initMap will pick up google.maps when loaded.
-*/
